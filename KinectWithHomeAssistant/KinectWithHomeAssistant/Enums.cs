1cf5f870d6a69e2ed6da58340f39501b9be7fbf2
connor roggero <connor roggero>
2016-05-01 00:59:35 -0400

    Secondary Push

--- KinectWithHomeAssistant/KinectWithHomeAssistant/Enums.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace KinectWithHomeAssistant
-{
-    public enum Gesture { TurnOnRightPointing, TurnOnLeftPointing, TurnOffRightPointing, TurnOffLeftPointing, none};
-    public enum RightHand { RightAboveHead, LeftAboveHead, BelowHead, AboveHeadRightToLeft, AboveHeadLeftToRight, Reset };
-    public enum LeftHand { RightAboveHead, LeftAboveHead, BelowHead, AboveHeadRightToLeft, AboveHeadLeftToRight, Reset }
-}
